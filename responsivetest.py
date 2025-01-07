2a0525bf9afbc6d91b51b708ef76e9d8aea3d602
Nihat Rzayev <143103414+RzayevNihat@users.noreply.github.com>
2025-01-07 23:15:00 +0400

    Add files via upload

--- responsivetest.py
+++ responsivetest.py
@@ -19,24 +19,6 @@ def test_responsive_view(width, height):
     driver.set_window_size(width, height)  # Pəncərə ölçüsünü təyin edir
     time.sleep(2)
     
-    try:
-        # Menü açılıb-açılmadığını yoxla
-        menu_button = driver.find_element(By.XPATH, '//button[@aria-label="Open navigation"]')
-        menu_button.click()
-        print(f"{width}x{height} üçün menyu düyməsi görünür və işləyir.")
-        time.sleep(2)
-    except Exception as e:
-        print(f"{width}x{height} ölçüsündə menyu düyməsi tapılmadı: {e}")
-
-    # Popular düyməsini yoxla
-    try:
-        popular_button = driver.find_element(By.LINK_TEXT, "Popular")
-        popular_button.click()
-        print(f"{width}x{height} üçün 'Popular' düyməsi işləyir.")
-        time.sleep(2)
-    except Exception as e:
-        print(f"{width}x{height} ölçüsündə 'Popular' düyməsi tapılmadı: {e}")
-
 # Responsivlik üçün ölçülər
 responsive_sizes = [
     (1920, 1080),  # Desktop
@@ -48,17 +30,6 @@ responsive_sizes = [
 for size in responsive_sizes:
     test_responsive_view(size[0], size[1])
 
-# Log Out
-try:
-    profile_menu = driver.find_element(By.XPATH, '//*[@id="expand-user-drawer-button"]')
-    profile_menu.click()
-    time.sleep(2)
-    
-    logout_button = driver.find_element(By.XPATH, '//*[@id="logout-list-item"]/div')
-    logout_button.click()
-    print("Çıxış uğurla tamamlandı.")
-except Exception as e:
-    print(f"Çıxış zamanı problem: {e}")
 
 # Brauzeri bağla
 time.sleep(3)
